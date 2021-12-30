49895645c0105ab23070ba19f77b783de19cb483
FDGarciaC <franciscodanielgarcia@ibermutua.es>
2021-12-30 18:03:29 +0100

    Changed icons

--- app/src/main/java/com/fdanielgarcia/gc9kt3a/MainActivity.kt
+++ app/src/main/java/com/fdanielgarcia/gc9kt3a/MainActivity.kt
@@ -5,7 +5,6 @@ import android.location.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.fdanielgarcia.gc9kt3a.R
 
 class MainActivity : AppCompatActivity(), LocationListener {
     val TIEMPO_MIN = (10 * 1000).toLong() // 10 segundos
